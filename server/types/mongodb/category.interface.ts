6d000f9d02f82d9d6c0e84eda02d3e5bb68e08e4
Michal Parchanowicz <michal.parchanowicz@gmail.com>
2024-08-03 17:43:52 +0200

    feat: Adding productCount to getCategory

--- server/types/mongodb/category.interface.ts
+++ server/types/mongodb/category.interface.ts
@@ -1,6 +1,7 @@
 import { Document, Types } from "mongoose";
+import { ISubcategory } from "./subcategory.interface";
 
 export interface ICategory extends Document {
     name: string;
-    subcategories: Types.ObjectId[];
+    subcategories: Types.ObjectId[] | ISubcategory[];
 }
