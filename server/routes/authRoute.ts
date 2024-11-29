563a9a8b589deae422b1075ce58429d23fcd4192
Michal_Parchanowicz <michal.parchanowicz@gmail.com>
2024-11-29 21:26:24 +0100

    feat: adding user info endpoint

--- server/routes/authRoute.ts
+++ server/routes/authRoute.ts
@@ -1,11 +1,11 @@
 import express from "express";
-import { registerUser, loginUser, getCurrentUser } from "../controllers/authController";
+import { registerUser, loginUser, getUserInfo } from "../controllers/authController";
 import validateToken from "../middleware/validateTokenHandler";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-router.get("/user", validateToken, getCurrentUser);
+router.get("/user", validateToken, getUserInfo);
 
 export default router;
